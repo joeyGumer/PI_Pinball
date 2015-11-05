b39699aaa575f740c8d7d9a4b6a6edb42313501d
joeyGumer <pep3553@gmail.com>
2015-11-05 16:43:35 +0100

    Nou pinball

--- Pinballv2/Globals.h
+++ Pinballv2/Globals.h
@@ -21,8 +21,8 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 410
-#define SCREEN_HEIGHT 650
+#define SCREEN_WIDTH 448
+#define SCREEN_HEIGHT 734
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
 #define WIN_RESIZABLE false
